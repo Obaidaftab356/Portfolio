55471cd9f6f8f0aafe2e59b5218ddc38bf126292
Obaid Aftab <obaidaftab123@gmail.com>
2026-09-02 13:44:35 +0500

    Fix App.js build errors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import './App.css'
-import {FaGithub , FaLinkedin , FaReact} from "react-icons/fa"
-import Navbar from './Components/Navbar'
-import {BrowserRouter , Routes,Route, Link} from 'react-router-dom'
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Projects from "./Pages/Projects";
-import Resume from "./Pages/Resume";
-import Contact from "./Pages/Contact";
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Projects from './Pages/Projects';
+import Resume from './Pages/Resume';
+import Contact from './Pages/Contact';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element= {<Home />}/>
-      <Route path='/about' element= {<About />}/>
-      <Route path='/projects' element= {<Projects />}/>
-      <Route path='/resume' element= {<Resume  />}/>
-      <Route path='/contact' element= {<Contact />}/>
-    </Routes>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
-    
-  )
+  );
 }
